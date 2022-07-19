bebec0b792990e513249b36d757444457a670168
crashdown <thecrashdownrus@gmail.com>
2022-07-19 19:30:13 +0300

    add buttons plugs with styles

--- src/components/list-item/list-item.js
+++ src/components/list-item/list-item.js
@@ -3,7 +3,18 @@ import React from "react";
 import './list-item.css'
 
 const ListItem = ({ label }) => {
-    return <span className="list-group-item">{label}</span>
+    return <div className="list-group-item">
+        <span className="todo-item-text">{label}</span>
+
+        <button type="button" className="btn btn-sm todo-item-remove-button">
+            <i class="bi bi-trash"></i>
+        </button>
+
+        <button type="button" className="btn btn-sm todo-item-pin-button">
+            <i class="bi bi-pin-angle"></i>
+        </button>
+        
+    </div>
 }
 
 export default ListItem
